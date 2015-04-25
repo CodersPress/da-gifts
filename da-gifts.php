9cab7f0a5d083707dc96f1d89db524cc0ecb51f4
Martin <support@coderspress.com>
2015-04-25 13:49:38 +0100

    updated

--- da-gifts.php
+++ da-gifts.php
@@ -3,8 +3,8 @@
 Plugin Name: Dating Theme Gifts Extended
 Plugin URI: http://coderspress.com
 Description: Extended set of gift images for Premiumpress - Dating Theme
-Version: 1.0
-Revision Date: 17th April 2015
+Version: 2.0
+Revision Date: 25th April 2015
 Author: sMarty
 Author URI: http://coderspress.com
 License: http://creativecommons.org/licenses/GPL/2.0
