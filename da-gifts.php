e7469af87a8b07b633fb65d099279cff4ce67f59
Martin <support@coderspress.com>
2015-04-25 05:18:37 +0100

    updated

--- da-gifts.php
+++ da-gifts.php
@@ -7,29 +7,8 @@ Version: 2.0
 Revision Date: 17th April 2015
 Author: sMarty
 Author URI: http://coderspress.com
-License: GPLv2
+License: http://creativecommons.org/licenses/GPL/2.0
 */
-/**
- *
- * GNU General Public License, Free Software Foundation
- * <http://creativecommons.org/licenses/GPL/2.0/>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- */
-/*************************************************************************************************************/
 
 add_action( 'init', 'dag_plugin_updater' );
 function dag_plugin_updater() {
@@ -57,7 +36,6 @@ function dag_plugin_updater() {
 	    }
     }
 }
-
 
 /* Version - only used for first time install */ 
 define ( 'DA_GIFTS_DB_VERSION', '1' );
