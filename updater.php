3b0f875f84458dd27ce38f9c501eef961e526a66
Martin <support@coderspress.com>
2015-05-13 16:18:51 +0100

    update

--- updater.php
+++ updater.php
@@ -354,6 +354,17 @@ class WP_DAG_UPDATER {
 
 			global $wp_filesystem;
 
+			// Restore images
+			$from = ABSPATH.'da_backup_images';
+            $to = $result['destination'].'includes/images/';
+
+			$wp_filesystem->move( $from, $to );
+			$fail  = __( 'Could not restore images.<br>', 'github_plugin_updater' );
+			$success = __( 'Restoring Images...<br>', 'github_plugin_updater' );
+			echo is_wp_error( $wp_filesystem ) ? $fail : $success;
+			$wp_filesystem->delete($from, true);
+
+
 			// Move & Activate
 			$proper_destination = WP_PLUGIN_DIR.'/'.$this->config['proper_folder_name'];
 
@@ -366,18 +377,6 @@ class WP_DAG_UPDATER {
 			$fail  = __( 'The plugin has been updated, but could not be reactivated. Please reactivate it manually.<br>', 'github_plugin_updater' );
 			$success = __( 'Plugin reactivated successfully.<br>', 'github_plugin_updater' );
 			echo is_wp_error( $activate ) ? $fail : $success;
-
-
-			// Restore images
-			$from = ABSPATH.'da_backup_images';
-            $to = WP_PLUGIN_DIR.'/'.$this->config['proper_folder_name'].'/includes/images/';
-
-			$wp_filesystem->move( $from, $to );
-			$fail  = __( 'Could not restore images.<br>', 'github_plugin_updater' );
-			$success = __( 'Restoring Images...<br>', 'github_plugin_updater' );
-			echo is_wp_error( $wp_filesystem ) ? $fail : $success;
-			$wp_filesystem->delete($from, true);
-
 
 			return $result;
 	}
